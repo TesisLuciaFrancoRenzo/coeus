c936bc82e3c92023f5bbb563b92ccb2f48074460
Franco Pellegrini <francogpellegrini@gmail.com>
2015-06-13 10:10:02 -0300

    TestSuit completo

--- test/TestSuite.java
+++ test/TestSuite.java
@@ -4,9 +4,22 @@
  * and open the template in the editor.
  */
 
+import ar.edu.unrc.tdlearning.perceptron.learning.ActionPredictionTest;
+import ar.edu.unrc.tdlearning.perceptron.learning.MaximalListConsumerTest;
+import ar.edu.unrc.tdlearning.perceptron.learning.StateProbabilityTest;
+import ar.edu.unrc.tdlearning.perceptron.learning.TDLambdaLearningAfterstateTest;
+import ar.edu.unrc.tdlearning.perceptron.learning.TDLambdaLearningStateTest;
 import ar.edu.unrc.tdlearning.perceptron.learning.TDLambdaLearningTest;
+import ar.edu.unrc.tdlearning.perceptron.ntuple.ComplexNTupleComputationTest;
 import ar.edu.unrc.tdlearning.perceptron.ntuple.NTupleSystemTest;
 import ar.edu.unrc.tdlearning.perceptron.training.CircularCustomQueueTest;
+import ar.edu.unrc.tdlearning.perceptron.training.FunctionUtilsTest;
+import ar.edu.unrc.tdlearning.perceptron.training.LayerTest;
+import ar.edu.unrc.tdlearning.perceptron.training.NeuralNetCacheTest;
+import ar.edu.unrc.tdlearning.perceptron.training.NeuronTest;
+import ar.edu.unrc.tdlearning.perceptron.training.PartialNeuronTest;
+import ar.edu.unrc.tdlearning.perceptron.training.TDTrainerNTupleSystemTest;
+import ar.edu.unrc.tdlearning.perceptron.training.TDTrainerPerceptronTest;
 import ar.edu.unrc.tdlearning.perceptron.training.TDTrainerTest;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -20,7 +33,25 @@ import org.junit.runners.Suite;
  * @author lucia bressan, franco pellegrini, renzo bianchini
  */
 @RunWith( Suite.class )
-@Suite.SuiteClasses( {TDTrainerTest.class, CircularCustomQueueTest.class, TDLambdaLearningTest.class, NTupleSystemTest.class} )
+@Suite.SuiteClasses(
+         {TDTrainerTest.class,
+            CircularCustomQueueTest.class,
+            TDLambdaLearningTest.class,
+            NTupleSystemTest.class,
+            TDTrainerPerceptronTest.class,
+            TDTrainerNTupleSystemTest.class,
+            PartialNeuronTest.class,
+            NeuronTest.class,
+            NeuralNetCacheTest.class,
+            LayerTest.class,
+            FunctionUtilsTest.class,
+            NTupleSystemTest.class,
+            ComplexNTupleComputationTest.class,
+            TDLambdaLearningStateTest.class,
+            TDLambdaLearningAfterstateTest.class,
+            StateProbabilityTest.class,
+            MaximalListConsumerTest.class,
+            ActionPredictionTest.class} )
 public class TestSuite {
 
     /**
