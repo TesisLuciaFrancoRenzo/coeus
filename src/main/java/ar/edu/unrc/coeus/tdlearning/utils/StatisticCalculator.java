8639823147cac042b30fbcc546d256f226ac2e19
Franco Gastón Pellegrini <francogpellegrini@gmail.com>
2016-12-16 16:20:53 -0300

    Unificación de cálculos de estadisticas

--- src/main/java/ar/edu/unrc/coeus/tdlearning/utils/StatisticCalculator.java
+++ src/main/java/ar/edu/unrc/coeus/tdlearning/utils/StatisticCalculator.java
@@ -1,27 +1,138 @@
 package ar.edu.unrc.coeus.tdlearning.utils;
 
+import java.text.DecimalFormat;
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 /**
  * Created by franc on 16/12/2016.
  */
 public
 class StatisticCalculator {
-    long counter;
-    private double average;
+    private final int             capacity;
+    private final DecimalFormat   formatter;
+    private final Queue< Double > history;
+    private final int             outputDecimals;
+    private       double          average;
+    private       int             itemCounter;
+
+    public
+    StatisticCalculator(
+            int capacity,
+            int outputDecimals
+    ) {
+
+        this.outputDecimals = outputDecimals;
+        this.average = 0;
+        this.itemCounter = 0;
+        if ( capacity > 0 ) {
+            this.capacity = capacity;
+            this.history = new ArrayDeque<>(capacity);
+        } else {
+            this.capacity = 0;
+            this.history = null;
+        }
+        if ( outputDecimals > 0 ) {
+            StringBuilder pattern = new StringBuilder(outputDecimals);
+            for ( int i = 0; i < outputDecimals; i++ ) {
+                pattern.append('#');
+            }
+            formatter = new DecimalFormat("#." + pattern.toString());
+        } else if ( outputDecimals == 0 ) {
+            formatter = new DecimalFormat("#"); //TODO testear
+        } else {
+            formatter = null;
+        }
+    }
 
     public
     StatisticCalculator() {
-        average = 0;
-        counter = 0;
+        this(0, -1);
+    }
+
+    public
+    StatisticCalculator( int outputDecimals ) {
+        this(0, outputDecimals);
     }
 
     public synchronized
     void addSample( double sample ) {
-        average += sample;
-        counter++;
+        if ( history != null ) {
+            if ( history.size() >= capacity ) {
+                average -= history.remove();
+                itemCounter--;
+            }
+            history.add(sample);
+            average += sample;
+            itemCounter++;
+            assert itemCounter <= capacity;
+        } else {
+            average += sample;
+            itemCounter++;
+        }
     }
 
     public
     double getAverage() {
-        return average / ( counter * 1d );
+        return average / ( itemCounter * 1d );
+    }
+
+    public
+    int getCapacity() {
+        return capacity;
+    }
+
+    public
+    Double getFullCapacityAverage() {
+        if ( itemCounter == capacity ) {
+            return getAverage();
+        } else {
+            return null;
+        }
+    }
+
+    public
+    int getItemCounter() {
+        return itemCounter;
+    }
+
+    public
+    int getOutputDecimals() {
+        return outputDecimals;
+    }
+
+    public
+    String printableAverage() {
+        if ( itemCounter < 1 ) {
+            return "?";
+        } else {
+            if ( formatter != null ) {
+                return formatter.format(getAverage());
+            } else {
+                return Double.toString(getAverage());
+            }
+        }
+    }
+
+    public
+    String printableFullCapacityAverage() {
+        if ( itemCounter < capacity ) {
+            return "?";
+        } else {
+            if ( formatter != null ) {
+                return formatter.format(getAverage());
+            } else {
+                return Double.toString(getAverage());
+            }
+        }
+    }
+
+    public
+    void reset() {
+        average = 0;
+        itemCounter = 0;
+        if ( history != null ) {
+            history.clear();
+        }
     }
 }
--- src/test/java/ar/edu/unrc/coeus/tdlearning/utils/StatisticCalculatorTest.java
+++ src/test/java/ar/edu/unrc/coeus/tdlearning/utils/StatisticCalculatorTest.java
@@ -12,6 +12,33 @@ public
 class StatisticCalculatorTest {
     @Test
     public
+    void addSample()
+            throws Exception {
+        StatisticCalculator estimator = new StatisticCalculator(5, 2);
+        estimator.addSample(10.4);
+        estimator.addSample(4);
+        assertThat(estimator.getItemCounter(), is(2));
+
+        estimator.addSample(5);
+        estimator.addSample(9);
+        assertThat(estimator.getItemCounter(), is(4));
+
+        estimator.addSample(2);
+        assertThat(estimator.getItemCounter(), is(5));
+
+        estimator.addSample(1);
+        assertThat(estimator.getItemCounter(), is(5));
+        estimator.addSample(1);
+        estimator.addSample(1);
+        estimator.addSample(1);
+        estimator.addSample(1);
+        assertThat(estimator.getItemCounter(), is(5));
+
+        assertThat(estimator.getAverage(), is(1d));
+    }
+
+    @Test
+    public
     void getAverage()
             throws Exception {
         StatisticCalculator sc = new StatisticCalculator();
@@ -22,6 +49,66 @@ class StatisticCalculatorTest {
         sc.addSample(-26.5);
         sc.addSample(0);
         assertThat(sc.getAverage(), is(2.027633333333334d));
+    }
+
+    @Test
+    public
+    void getFullCapacityAverage()
+            throws Exception {
+
+    }
+
+    @Test
+    public
+    void printableAverage()
+            throws Exception {
+
+    }
+
+    @Test
+    public
+    void printableFullCapacityAverage()
+            throws Exception {
+        StatisticCalculator estimator = new StatisticCalculator(5, 2);
+        estimator.addSample(10.4);
+        estimator.addSample(1.5);
+        assertThat(estimator.printableFullCapacityAverage(), is("?"));
+
+        estimator.addSample(5);
+        estimator.addSample(9);
+        assertThat(estimator.printableFullCapacityAverage(), is("?"));
+
+        estimator.addSample(2);
+        assertThat(estimator.printableFullCapacityAverage(), is("5,58"));
+
+        estimator.addSample(2);
+        estimator.addSample(2);
+        estimator.addSample(3);
+        estimator.addSample(2);
+        estimator.addSample(3);
+        assertThat(estimator.printableFullCapacityAverage(), is("2,4"));
+
+        estimator = new StatisticCalculator(30, 2);
+        for ( int i = 0; i < 29; i++ ) {
+            estimator.addSample(0);
+        }
+        estimator.addSample(3.2);
+        assertThat(estimator.printableFullCapacityAverage(), is("0,11"));
+    }
+
+    @Test
+    public
+    void reset()
+            throws Exception {
+        StatisticCalculator estimator = new StatisticCalculator(5, 2);
+        estimator.addSample(10.4);
+        estimator.addSample(4);
+        estimator.addSample(5);
+        estimator.addSample(9);
+        assertThat(estimator.getItemCounter(), is(4));
+
+        estimator.reset();
+        assertThat(estimator.getItemCounter(), is(0));
     }
 
 }
